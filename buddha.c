92df051dc65175eec2e52c40870fe70cbc058935
bsekisser <squirmyworms@embarqmail.com>
2021-09-13 10:33:46 -0400

    Initial commit
    	new file:   .gitignore
    	new file:   buddha-0x000000-0x0005ff
    	new file:   buddha-0x000600-0x007dff
    	new file:   buddha-0x007e00-0x008dff
    	new file:   buddha-0x008e00-0x0096ff
    	new file:   buddha-0x009700-0x200000
    	new file:   buddha.bin
    	new file:   buddha.c
    	new file:   buddha.raw
    	new file:   readme.txt

---
+++ buddha.c
@@ -0,0 +1,70 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int fd_copy(const char* path, uint8_t* raw, uint32_t start, uint32_t end)
+{
+	char buf[256];
+	snprintf(buf, 0xff, "%s-0x%06x-0x%06x", path, start, end);
+
+	int do_write = 1;
+
+	int wfd = 0;
+	if(do_write) {
+		wfd = creat(buf, O_WRONLY);
+		if(-1 == wfd)
+			{ perror("fd_copy -- open"); return(-1); }
+	}
+
+	uint8_t* src = &raw[start];
+	uint32_t len = end - start;
+	
+	printf("%s, end = 0x%08x, len = 0x%08x\n", buf, end - start, len);
+
+	if(do_write) {
+		int err = write(wfd, src, len);
+		if(-1 == wfd)
+			{ perror("fd_copy -- write"); return(-1); }
+	
+		close(wfd);
+	}
+
+	return(0);
+}
+
+int main(void)
+{
+	int fd = open("buddha.bin", O_RDONLY);
+	if(-1 == fd)
+		{ perror("open"); return(-1); }
+	
+	struct stat sb;
+	if(-1 == fstat(fd, &sb))
+		{ perror("stat"); return(-1); }
+	
+	uint8_t* bin = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	if(MAP_FAILED == bin)
+		{ perror("mmap"); return(-1); }
+	
+	close(fd);
+	
+	/* **** */
+
+	fd_copy("buddha", bin, 0x0000, 0x05ff);
+	fd_copy("buddha", bin, 0x0600, 0x7dff);
+	fd_copy("buddha", bin, 0x7e00, 0x8dff);
+	fd_copy("buddha", bin, 0x8e00, 0x96ff);
+	fd_copy("buddha", bin, 0x9700, sb.st_size);
+	
+	/* **** */
+
+	munmap(bin, sb.st_size);
+	
+	return(0);
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=LNpbvyLIvN0
+
+www.bigclive.com/buddha.bin
